5c00dd0cdbce06cb55d0f0c346d5d3fcc8566ea2
alex <alex>
2018-12-29 15:10:37 +0100

    Sviluppo della lettura della immagine.

--- BoundingBoxes.cpp
+++ BoundingBoxes.cpp
@@ -19,8 +19,38 @@ public:
     int thresh = 100;
     RNG rng = RNG(12345);
 
+
+
+
+
+
+
     void workOnThisPage(int indexpage)
     {
+        printf(" workOnThisPage %d  \n ",indexpage);
+        if(indexpage ==1)
+        {
+            printf(" for now I want to work on just one image %d  \n ",indexpage);
+            Mat src = imread( "../data/UnieuroSecondaPagina.png" );
+            if( src.empty() )
+            {
+                printf("Could not open or find the image!\n");
+            }
+            cvtColor( src, src_gray, COLOR_BGR2GRAY );
+            blur( src_gray, src_gray, Size(3,3) );
+            const char* source_window = "Source";
+            namedWindow( source_window );
+            imshow( source_window, src );
+            const int max_thresh = 255;
 
+            //createTrackbar( "Canny thresh:", source_window, &thresh, max_thresh, thresh_callback );
+            //thresh_callback( 0, 0 );
+
+
+            waitKey();
+
+
+
+        }
     }
 };
