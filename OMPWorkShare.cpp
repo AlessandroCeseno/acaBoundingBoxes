b7e063783ea2a0f00bf754ecefce1e17741bb3d4
alex <alex>
2018-12-29 13:12:03 +0100

    make

--- OMPWorkShare.cpp
+++ OMPWorkShare.cpp
@@ -2,6 +2,9 @@
 // Created by Alex - Team LEAN on 2018-12-29.
 //
 
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 class OMPWorkShare
         {
@@ -14,8 +17,15 @@ public:
         int nthreads, tid, i, chunk;
         int j;
 
-#pragma omp parallel shared(a,b,c,nthreads,chunk) private(i,tid)
+#pragma omp parallel shared(nthreads,chunk) private(i,tid)
         {
+            tid = omp_get_thread_num();
+            if (tid == 0)
+            {
+                nthreads = omp_get_num_threads();
+                printf("Number of threads = %d\n", nthreads);
+            }
+            printf("Thread %d starting...\n",tid);
 
         }
     }
