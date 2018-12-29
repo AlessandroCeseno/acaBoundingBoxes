2b3aec07cb29b633cdd837782de7cf8e1d16cc0d
alex <alex>
2018-12-29 12:08:16 +0100

    Inizializzazione di variabili

---
+++ OMPWorkShare.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Alex - Team LEAN on 2018-12-29.
+//
+
+
+class OMPWorkShare
+        {
+
+public:
+    int nthreads, tid, i, chunk;
+    int j;
+
+    void meth()
+    {
+
+
+    }
+};
