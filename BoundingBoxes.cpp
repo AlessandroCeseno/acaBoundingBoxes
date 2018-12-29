24248ceb41a124282ae26016356fe8d388311c0b
alex <alex>
2018-12-29 15:12:55 +0100

    Sviluppo Contorni e Rettangoli.

--- BoundingBoxes.cpp
+++ BoundingBoxes.cpp
@@ -19,6 +19,34 @@ public:
     int thresh = 100;
     RNG rng = RNG(12345);
 
+    void thresh_meth()
+    {
+        Mat canny_output;
+        Canny( src_gray, canny_output, thresh, thresh*2 );
+        vector<vector<Point> > contours;
+        findContours( canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE );
+        vector<vector<Point> > contours_poly( contours.size() );
+        vector<Rect> boundRect( contours.size() );
+        vector<Point2f>centers( contours.size() );
+        vector<float>radius( contours.size() );
+        for( size_t i = 0; i < contours.size(); i++ )
+        {
+            approxPolyDP( contours[i], contours_poly[i], 3, true );
+            boundRect[i] = boundingRect( contours_poly[i] );
+            minEnclosingCircle( contours_poly[i], centers[i], radius[i] );
+        }
+        Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+        for( size_t i = 0; i< contours.size(); i++ )
+        {
+            Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
+            drawContours( drawing, contours_poly, (int)i, color );
+            rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2 );
+            circle( drawing, centers[i], (int)radius[i], color, 2 );
+        }
+        imshow( "Contours", drawing );
+        //imwrite( "second.jpg", drawing );
+
+    }
 
 
 
@@ -45,7 +73,7 @@ public:
 
             //createTrackbar( "Canny thresh:", source_window, &thresh, max_thresh, thresh_callback );
             //thresh_callback( 0, 0 );
-
+            thresh_meth();
 
             waitKey();
 
