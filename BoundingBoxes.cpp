a313a47c9105453968971d80ff71739d80349096
alex <alex>
2018-12-30 09:42:20 +0100

    Fatta pulizia del codice

--- BoundingBoxes.cpp
+++ BoundingBoxes.cpp
@@ -28,13 +28,10 @@ public:
         findContours( canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE );
         vector<vector<Point> > contours_poly( contours.size() );
         vector<Rect> boundRect( contours.size() );
-        //vector<Point2f>centers( contours.size() );
-        //vector<float>radius( contours.size() );
         for( size_t i = 0; i < contours.size(); i++ )
         {
             approxPolyDP( contours[i], contours_poly[i], 3, true );
             boundRect[i] = boundingRect( contours_poly[i] );
-            //minEnclosingCircle( contours_poly[i], centers[i], radius[i] );
         }
         Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
         for( size_t i = 0; i< contours.size(); i++ )
@@ -42,10 +39,8 @@ public:
             Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
             drawContours( drawing, contours_poly, (int)i, color );
             rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2 );
-            //circle( drawing, centers[i], (int)radius[i], color, 2 );
         }
         //imshow( "Contours", drawing );
-
         imwrite("../outputacaBoundingBoxes/" + pagenumber + ".jpg", drawing );
     }
 
@@ -53,28 +48,23 @@ public:
     void workOnThisPage(int indexpage)
     {
         printf(" workOnThisPage %d  \n ",indexpage);
-        //if(indexpage ==1)
-        //{
-            printf(" for now I want to work on just one image at time  %d  \n ",indexpage);
-            std::string indexpageasstring = std::to_string(indexpage);
-            Mat src = imread( "../dataFlyers/" + indexpageasstring + ".png" );
-            if( src.empty() )
-            {
-                printf("Could not open or find the image!\n");
-            }
-            cvtColor( src, src_gray, COLOR_BGR2GRAY );
-            blur( src_gray, src_gray, Size(3,3) );
-            //show source window
-            //const char* source_window = "Source";
-            //namedWindow( source_window );
-            //imshow( source_window, src );
-            //const int max_thresh = 300;
+        printf(" for now I want to work on just one image at time  %d  \n ",indexpage);
+        std::string indexpageasstring = std::to_string(indexpage);
+        Mat src = imread( "../dataFlyers/" + indexpageasstring + ".png" );
+        if( src.empty() )
+        {
+            printf("Could not open or find the image!\n");
+        }
+        cvtColor( src, src_gray, COLOR_BGR2GRAY );
+        blur( src_gray, src_gray, Size(3,3) );
+        //show source window
+        //const char* source_window = "Source";
+        //namedWindow( source_window );
+        //imshow( source_window, src );
+        //const int max_thresh = 300;
 
-            //      createTrackbar( "Canny thresh:", source_window, &thresh, max_thresh, thresh_callback );
-            //      thresh_callback( 0, 0 );
-            thresh_meth(indexpageasstring);
-
-            //waitKey();
-        //}
+        //      createTrackbar( "Canny thresh:", source_window, &thresh, max_thresh, thresh_callback );
+        //      thresh_callback( 0, 0 );
+        thresh_meth(indexpageasstring);
     }
 };
