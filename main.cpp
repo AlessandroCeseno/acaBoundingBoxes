2475406822503f8373b32e04bfedfed8eba1f94e
alex <alex>
2019-01-01 17:30:15 +0100

    Inseriro il log per il tempo di esecuzione.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,17 @@ int main()
 {
     std::cout << "Hello Advanced Computer Architecture. Here my smart and simple project that works on Computer Vision! " << std::endl;
 
+    clock_t start, stop;
+    start = clock();
+
     OMPWorkShare workShare;
     workShare.elaborate();
+
+    stop = clock();
+    double elapsed_seconds = ((double) stop - start )/CLOCKS_PER_SEC;
+    printf("Time elapsed in seconds: %f \n", elapsed_seconds);
+    double elapsed_milliseconds = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("Time elapsed in milliseconds: %f \n", elapsed_milliseconds);
 
     return 0;
 }
