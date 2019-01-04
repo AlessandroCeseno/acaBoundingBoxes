da1d91ce590250d2943d06f0f2702e44cb0ef3ae
alex <alex>
2019-01-04 09:03:53 +0100

    Ho settato il tempo a RunTime e non a CPU Time

--- main.cpp
+++ main.cpp
@@ -5,17 +5,27 @@ int main()
 {
     std::cout << "Hello Advanced Computer Architecture. Here my smart and simple project that works on Computer Vision! " << std::endl;
 
-    clock_t start, stop;
-    start = clock();
+    double startTime = omp_get_wtime();
+
+    //clock_t start, stop;
+    //start = clock();
 
     OMPWorkShare workShare;
     workShare.elaborate();
 
+    double endTime = omp_get_wtime();		// registers the second time
+    double tottime = endTime-startTime;
+    // PRINT RESULTS
+    printf("Seconds: %f ", tottime);
+
+    /*
     stop = clock();
     double elapsed_seconds = ((double) stop - start )/CLOCKS_PER_SEC;
     printf("Time elapsed in seconds: %f \n", elapsed_seconds);
     double elapsed_milliseconds = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
     printf("Time elapsed in milliseconds: %f \n", elapsed_milliseconds);
+    */
+
 
     return 0;
 }
